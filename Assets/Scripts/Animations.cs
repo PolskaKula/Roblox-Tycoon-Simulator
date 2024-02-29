303ce0a0f9abccc22fbe0c075e826b54e918060c
PolskaKula <polska.kulae8@gmail.com>
2024-02-29 17:06:15 +0100

    .

--- Assets/Scripts/Animations.cs
+++ Assets/Scripts/Animations.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class Animations : MonoBehaviour
@@ -10,9 +11,20 @@ public class Animations : MonoBehaviour
     [SerializeField] private Animator Rleg;
 
     private bool isWalking;
+    private bool isWalking2;
+    private bool isJumping;
 
-    void Update()
+    async void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (!isJumping)
+            {
+                StartJumping();
+                await Task.Delay(800);
+                StopJumping();
+            }
+        }
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
         {
             if (!isWalking)
@@ -36,6 +48,7 @@ public class Animations : MonoBehaviour
         Lleg.SetTrigger("Walk");
         Rleg.SetTrigger("Walk");
         isWalking = true;
+        isWalking2 = true;
     }
 
     void StopWalking()
@@ -45,5 +58,22 @@ public class Animations : MonoBehaviour
         Lleg.SetTrigger("Idle");
         Rleg.SetTrigger("Idle");
         isWalking = false;
+        isWalking2 = false;
+    }
+
+    void StartJumping()
+    {
+        Larm.SetTrigger("Jump");
+        Rarm.SetTrigger("Jump");
+        isJumping = true;
+        isWalking2 = false;
+    }
+
+    void StopJumping()
+    {
+        Larm.SetTrigger("Fall");
+        Rarm.SetTrigger("Fall");
+        isJumping = false;
+        isWalking2 = false;
     }
 }
--- Assets/Scripts/BeginWorking.cs
+++ Assets/Scripts/BeginWorking.cs
@@ -6,6 +6,8 @@ public class BeginWorking : MonoBehaviour
 {
     [SerializeField] private int Cost;
     public bool bought;
+    [SerializeField] private AudioSource Body;
+    [SerializeField] private AudioClip Cash, Beep;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -16,6 +18,13 @@ public class BeginWorking : MonoBehaviour
                 GameManager.GM.Money -= Cost;
                 GameManager.GM.NumberOfUpgrades++;
                 bought = true;
+                Body.clip = Cash;
+                Body.Play();
+            }
+            else
+            {
+                Body.clip = Beep;
+                Body.Play();
             }
         }
     }
